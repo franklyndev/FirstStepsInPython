9a5e2412b5a2c6b96a8eb3092e1b78a3e6c2204e
Franklyndev <franklynsantos451@gmail.com>
2025-03-26 13:44:34 -0300

    first commit

---
+++ TypeSome.py
@@ -0,0 +1,8 @@
+some = input('Type something here: ')
+print(type(some))
+print("is numeric? ", some.isnumeric())
+print("is alpha? ", some.isalpha())
+print("is alnum? ", some.isalnum())
+print("is upper? ", some.isupper())
+print("is lower? ", some.islower())
+
---
+++ Variables.py
@@ -0,0 +1,4 @@
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter another number: "))
+sum_result = n1 + n2
+print(f"The result of the sum between {n1} and {n2} is ", sum_result)
