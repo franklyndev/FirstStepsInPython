61d0365de3e7516b2a52fa4c3db3e8fbe941c182
FranklynSantos <franklynsantos451@gmail.com>
2025-04-10 23:01:44 -0300

    Python practice 2

---
+++ DoubleTripleSquareroot.py
@@ -0,0 +1,7 @@
+# Make a algorithm that read a number and display its double, triple and Square Root.
+import math
+
+number = int(input("Enter a number: "))
+print(f"The double of {number} =",number*2)
+print(f"The triple of {number} =",number*3)
+print(f"The Square root of {number} =",math.sqrt(number))
