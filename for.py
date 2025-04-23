e9d473eb347a01faf2f8f4dbaae067dc434722cc
FranklynSantos <franklynsantos451@gmail.com>
2025-04-23 12:50:20 -0300

    usoforpy

---
+++ for.py
@@ -0,0 +1,10 @@
+for c in range(1, 6):
+    print(c)          # vai contar de 1 até 5 pois o último ele ignora
+print("------------------------")
+for c in range(6, 0, -1):
+    print(c)          # vai contar de 6 até 1
+                    # range(6, 0= intervalo)
+                    # range(6, 0, -1 = salto)
+print("------------------------")
+for c in range(0, 9, 2):
+    print(c)           # vai contar de 0 até 8 num salto de 2 casas
